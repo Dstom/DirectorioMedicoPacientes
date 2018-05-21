832becbe2849226528650e6fe847ee42c36cbd95
Dstom <31371366+Dstom@users.noreply.github.com>
2018-05-21 03:46:53 -0500

    Initial commit

---
+++ app/src/main/java/com/example/alberto/directoriomedico/addeditpaciente/AddEditPacienteActivity.java
@@ -0,0 +1,57 @@
+package com.example.alberto.directoriomedico.addeditpaciente;
+
+import android.os.Bundle;
+import android.os.PersistableBundle;
+import android.support.annotation.Nullable;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.View;
+import android.widget.Toast;
+
+import com.example.alberto.directoriomedico.MainActivity;
+import com.example.alberto.directoriomedico.R;
+
+/**
+ * Created by Tommy on 20/05/2018.
+ */
+
+public class AddEditPacienteActivity extends AppCompatActivity {
+
+    public static final int REQUEST_ADD_PACIENTE = 1;
+
+    @Override
+    public void onCreate( Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_add_edit_paciente);
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        String pacienteId = getIntent().getStringExtra(MainActivity.EXTRA_PACIENTE_ID);
+
+        //id del medico
+        String medicoId = getIntent().getStringExtra(MainActivity.EXTRA_MEDICO_ID);
+
+        setTitle(pacienteId == null? "Añadir Paciente" :"Editar Paciente");
+
+        AddEditPacienteFragment fragment = (AddEditPacienteFragment)getSupportFragmentManager().findFragmentById(R.id.add_edit_paciente_container);
+
+        if(fragment == null){
+            fragment = AddEditPacienteFragment.newInstance(pacienteId,medicoId);
+            getSupportFragmentManager()
+                    .beginTransaction()
+                    .add(R.id.add_edit_paciente_container,fragment)
+                    .commit();
+        }
+    }
+
+    @Override
+    public boolean onSupportNavigateUp(){
+        onBackPressed();
+        return true;
+    }
+}
